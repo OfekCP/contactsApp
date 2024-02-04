62e60e682450f28cf2643effa0555bd2026fc189
OfekCP <ofekzvulunov@gmail.com>
2024-02-04 15:04:47 +0200

    update

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -1,12 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default {
+  // other config options...
   server: {
     proxy: {
-      '/contacts': process.env.VITE_API_URL,
+      '/contacts': `http://localhost:8000`,
+      '/contacts': `https://contact-app-5czc.onrender.com`,
     },
   },
-});
+};
