7f74058ed24862c2376557018046d60249844393
OfekCP <ofekzvulunov@gmail.com>
2024-02-04 14:43:51 +0200

    update

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
 
   const fetchContacts = async () => {
     try {
-      const response = await axios.get('import.meta.env.VITE_API_URL/contacts');
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/contacts`);
       setContacts(response.data);
     } catch (error) {
       console.error(error);
