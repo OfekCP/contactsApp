0028d1186a1fc99259dd8b44d2e121c71dc294b7
OfekCP <ofekzvulunov@gmail.com>
2024-02-04 14:00:42 +0200

    update

--- frontend/front/src/main.jsx
+++ frontend/front/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import ContactList from './contactList.jsx'
-import CreateContact from './CreateContact.jsx'
-import phone from './iphone.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
