937313539c3432f0f62c57b38e40eedd0e6c5b94
OfekCP <ofekzvulunov@gmail.com>
2024-02-04 15:06:01 +0200

    update

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -6,8 +6,8 @@ export default {
   // other config options...
   server: {
     proxy: {
-      '/contacts': `http://localhost:8000`,
-      '/contacts': `https://contact-app-5czc.onrender.com`,
+      '/contacts': 'http://localhost:8000',
+      '/contacts': 'https://contact-app-5czc.onrender.com',
     },
   },
 };
